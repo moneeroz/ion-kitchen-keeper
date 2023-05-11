cb55bad2d1ff27f9db4ee2c843d67a18820f1d72
Moneer <moneer.alromimah@gmail.com>
2023-05-11 00:54:33 -0600

    added recipe service

---
+++ src/app/services/recipe.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Irecipe } from '../interfaces/irecipe';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  apiURL: string = 'http://localhost:2828/api/recipes';
+  constructor(private http: HttpClient) {}
+
+  getRecipes() {
+    return this.http.get<Irecipe[]>(this.apiURL);
+  }
+
+  getRecipe(recipe_id: string) {
+    return this.http.get<Irecipe>(this.apiURL + '/' + recipe_id);
+  }
+  createRecipe(recipe_data: any) {
+    return this.http.post<Irecipe>(this.apiURL + '/create-recipe', recipe_data);
+  }
+}
