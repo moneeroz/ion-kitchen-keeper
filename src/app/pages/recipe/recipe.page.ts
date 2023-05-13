3594cfeca074e09f2d4035ba413244c9b9f6fd37
Moneer <moneer.alromimah@gmail.com>
2023-05-13 16:13:40 -0600

    changed  the noNullOperator to optional operator

--- src/app/pages/recipe/recipe.page.ts
+++ src/app/pages/recipe/recipe.page.ts
@@ -10,7 +10,7 @@ import { RecipeService } from 'src/app/services/recipe.service';
 })
 export class RecipePage implements OnInit {
   recipe_id: string | null = '';
-  recipe!: Irecipe;
+  recipe?: Irecipe;
 
   constructor(
     private recipeService: RecipeService,
