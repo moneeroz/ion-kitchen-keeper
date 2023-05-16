b394190f11d4cc4165cb833726ac8d27c83a0d73
Moneer <moneer.alromimah@gmail.com>
2023-05-15 23:21:29 -0600

    refractored the favourite interface

--- src/app/interfaces/ifavourite.ts
+++ src/app/interfaces/ifavourite.ts
@@ -1,14 +1,8 @@
+import { Irecipe } from './irecipe';
+
 export interface Ifavourite {
   userId: string;
   recipeId: string;
   createdAt: string;
-  recipe: {
-    id: string;
-    name: string;
-    image: string;
-    cloudinary_id: string;
-    ingredients: string;
-    directions: string;
-    category_id: string;
-  };
+  recipe: Irecipe;
 }
