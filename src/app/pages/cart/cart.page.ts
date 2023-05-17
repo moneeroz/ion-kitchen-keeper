04f7ecc57e43cd0a542584696ccb17205d278651
Moneer <moneer.alromimah@gmail.com>
2023-05-16 21:27:49 -0600

    users can remove items from cart

--- src/app/pages/cart/cart.page.ts
+++ src/app/pages/cart/cart.page.ts
@@ -12,8 +12,15 @@ export class CartPage implements OnInit {
   constructor(private cartService: CartService) {}
 
   ionViewWillEnter() {
-    this.cart = this.cartService.CartItems;
-    console.log(this.cart);
+    this.cart = this.cartService.cartItems;
+    // console.log(this.cart);
   }
   ngOnInit() {}
+
+  onRemoveFromCart(item: IcartItem) {
+    this.cartService.removeFromCart(item);
+    this.cart = this.cartService.cartItems;
+    // console.log(this.cart);
+    alert('removed from cart');
+  }
 }
