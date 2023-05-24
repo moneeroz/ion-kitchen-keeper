0b1b4a1e58f5140f8de80db3f97b864ad6c56ddb
Moneer <moneer.alromimah@gmail.com>
2023-05-23 22:25:53 -0600

    add a header component to use across pages

---
+++ src/app/components/header/header.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule, IonicModule, RouterModule],
+  exports: [HeaderComponent],
+})
+export class HeaderModule {}
