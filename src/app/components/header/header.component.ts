64720f0bbaf6f8eb3500b96e8859874970c918f2
Moneer <moneer.alromimah@gmail.com>
2023-05-29 21:47:48 -0600

    show cart item quantity on header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { IcartItem } from 'src/app/interfaces/icart';
+import { Iuser } from 'src/app/interfaces/iuser';
+import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +12,25 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Input() title: string = '';
 
-  constructor() {}
+  user: Iuser;
+  cartItems!: IcartItem[];
+  quantity: number = 0;
+  constructor(private cart: CartService, private userService: UserService) {
+    this.user = this.userService.getUserData();
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getItems();
+  }
+  getItems() {
+    this.cart.getCartItems(this.user.id).subscribe({
+      next: (recipes) => {
+        this.cartItems = recipes;
+        this.quantity = recipes.length;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
