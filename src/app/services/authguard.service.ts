31b965f2c5e9a96de7712e2b0f1ed13f528a432f
Moneer <moneer.alromimah@gmail.com>
2023-06-17 16:42:19 -0600

    edit

--- src/app/services/authguard.service.ts
+++ src/app/services/authguard.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@angular/core';
 import { UserService } from './user.service';
 import { Router } from '@angular/router';
+import { Observable, of, switchMap, take } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { IappState } from 'src/store/iapp-state';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthguardService {
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private store: Store<IappState>,
+  ) {}
 
-  canActivate(): boolean {
-    if (!this.userService.isAuthenticated()) {
-      this.router.navigateByUrl('login');
-      return false;
-    }
-    return true;
+  // canActivate(): boolean {
+  //   if (!this.userService.isAuthenticated()) {
+  //     this.router.navigateByUrl('login');
+  //     return false;
+  //   }
+  //   return true;
+  // }
+
+  canActivate(): Observable<boolean> {
+    return this.store.select('login').pipe(
+      take(1),
+      switchMap((loginState) => {
+        if (loginState.isLoggedIn) {
+          return of(true);
+        }
+        this.router.navigateByUrl('login');
+        return of(false);
+      }),
+    );
   }
 }
