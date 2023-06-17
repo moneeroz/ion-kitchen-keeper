804c946fd82372046074265475eb69b944dde9cc
Moneer <moneer.alromimah@gmail.com>
2023-06-16 22:36:42 -0600

    edit

--- src/store/appStoreModule.ts
+++ src/store/appStoreModule.ts
@@ -5,12 +5,15 @@ import { EffectsModule } from '@ngrx/effects';
 import { LoginEffects } from './login/login.effects';
 import { registerReducer } from './register/register.reducers';
 import { RegisterEffects } from './register/register.effects';
+import { recipesReducer } from './recipes/recipes.reducer';
+import { RecipesEffects } from './recipes/recipes.effects';
 
 export const appStoreModule = [
   StoreModule.forRoot([]),
   StoreModule.forFeature('loading', loadingReducer),
   StoreModule.forFeature('login', loginReducer),
   StoreModule.forFeature('register', registerReducer),
+  StoreModule.forFeature('recipes', recipesReducer),
   EffectsModule.forRoot([]),
-  EffectsModule.forFeature([LoginEffects, RegisterEffects]),
+  EffectsModule.forFeature([LoginEffects, RegisterEffects, RecipesEffects]),
 ];
