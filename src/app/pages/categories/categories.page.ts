b8b9ffcf703be44f18d1a9a607a996f8bb7c7f23
Moneer <moneer.alromimah@gmail.com>
2023-06-20 21:13:31 -0600

    implement state to category page

--- src/app/pages/categories/categories.page.ts
+++ src/app/pages/categories/categories.page.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { Irecipe } from 'src/app/interfaces/irecipe';
 import { Iuser } from 'src/app/interfaces/iuser';
 import { FavouriteService } from 'src/app/services/favourite.service';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { UserService } from 'src/app/services/user.service';
+import { CategoryActions } from 'src/store/category/category.actions';
+import { selectCategoryItems } from 'src/store/category/category.selectors';
+import { IappState } from 'src/store/iapp-state';
 
 @Component({
   selector: 'app-categories',
@@ -14,35 +19,47 @@ import { UserService } from 'src/app/services/user.service';
 export class CategoriesPage implements OnInit {
   pageTitle: string = 'catagories';
   recipes: Irecipe[] = [];
-  user!: Iuser;
+
+  recipes$: Observable<Irecipe[]>;
+
+  // user!: Iuser;
   constructor(
-    private recipeService: RecipeService,
+    // private recipeService: RecipeService,
     private router: Router,
-    private userService: UserService,
-    private favouriteService: FavouriteService,
-  ) {}
+    // private userService: UserService,
+    // private favouriteService: FavouriteService,
+    private store: Store<IappState>,
+  ) {
+    this.recipes$ = this.store.select(selectCategoryItems);
+  }
 
   ngOnInit() {
-    this.user = this.userService.getUserData();
-    this.recipeService.getRecipesByCategory('chicken').subscribe({
-      next: (recipes) => {
-        this.recipes = recipes;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    // this.user = this.userService.getUserData();
+    // this.recipeService.getRecipesByCategory('chicken').subscribe({
+    //   next: (recipes) => {
+    //     this.recipes = recipes;
+    //   },
+    //   error: (err) => {
+    //     console.log(err);
+    //   },
+    // });
+
+    this.getRecipesByCategory('chicken');
   }
 
   getRecipesByCategory(category: string) {
-    this.recipeService.getRecipesByCategory(category).subscribe({
-      next: (recipes) => {
-        this.recipes = recipes;
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    // this.recipeService.getRecipesByCategory(category).subscribe({
+    //   next: (recipes) => {
+    //     this.recipes = recipes;
+    //   },
+    //   error: (err) => {
+    //     console.log(err);
+    //   },
+    // });
+
+    this.store.dispatch(
+      CategoryActions.getRecipesByCategoryRequest({ category }),
+    );
   }
 
   onView(recipe_id: string) {
@@ -51,16 +68,16 @@ export class CategoriesPage implements OnInit {
   }
 
   onFavourite(recipe_id: string) {
-    console.log(recipe_id);
-    this.favouriteService.addToFavourites(this.user.id, recipe_id).subscribe({
-      next: (result) => {
-        console.log(result);
-        alert('Recipe added to Favourites');
-      },
-      error: (err) => {
-        console.log(err);
-        alert('Recipe is already in Favourites');
-      },
-    });
+    // console.log(recipe_id);
+    // this.favouriteService.addToFavourites(this.user.id, recipe_id).subscribe({
+    //   next: (result) => {
+    //     console.log(result);
+    //     alert('Recipe added to Favourites');
+    //   },
+    //   error: (err) => {
+    //     console.log(err);
+    //     alert('Recipe is already in Favourites');
+    //   },
+    // });
   }
 }
